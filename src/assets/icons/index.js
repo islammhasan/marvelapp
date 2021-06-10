7df74d3bd178f1f2562c465eb6441c78295d658b
islammhasan <im.islam94@gmail.com>
2021-06-10 20:27:36 +0200

    added assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,4 @@
+export const icons = {
+  back: require('./ic-back.png'),
+  search: require('./ic-magnifying-glass.png'),
+};
---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+export const images = {
+  logo: require('./marvel-logo.png'),
+  background: require('./mcu-background.jpeg'),
+  placeholder: require('./image-placeholder.jpeg'),
+};
