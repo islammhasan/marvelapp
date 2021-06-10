3793e25ac528d6d5742d5244ca8e227a41a486ad
islammhasan <im.islam94@gmail.com>
2021-06-10 21:15:10 +0200

    updated colors

--- src/assets/colors/index.js
+++ src/assets/colors/index.js
@@ -1,3 +1,10 @@
 export const colors = {
-  black: '#000000',
+  black: '#131313',
+  white: '#e6eaf3',
+  gray: '#1b1b1b',
+  red: '#ff0004',
+  lightred: '#ff898e',
+  highlight: '#8e2025',
+  searchbar: '#2d2d2d',
+  charcard: '#2d2d2d',
 };
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, View, StyleSheet} from 'react-native';
 import {colors} from '../../assets/colors';
 
-export const Container = ({children}) => {
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+export const Container = ({children, style}) => {
+  return (
+    <SafeAreaView style={[styles.container, style]}>
+      <View style={[styles.container, style]}>{children}</View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
